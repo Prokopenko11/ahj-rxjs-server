6148d85efc5d1010241b3f5627927046facf6f03
Valeria Prokopenko <valeriaprokopenko59@gmail.com>
2025-02-11 23:31:28 +0300

    server logic changed

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const Koa = require('koa');
 const { koaBody } = require('koa-body');
-const { faker } = require('@faker-js/faker')
+const cors = require('@koa/cors');
+const { faker } = require('@faker-js/faker');
 
 const app = new Koa();
 
+app.use(cors());
 app.use(koaBody());
+
+let messages = [];
+
+function generateMessage() {
+  const newMessage = {
+    id: faker.string.uuid(),
+    from: faker.internet.email(),
+    subject: faker.lorem.sentence(),
+    body: faker.lorem.paragraph(),
+    received: Math.floor(Date.now() / 1000),
+  };
+  messages.push(newMessage);
+}
+
+setInterval(generateMessage, 3000);
 
 app.use(async (ctx) => {
   if (ctx.path === '/messages/unread' && ctx.method === 'GET') {
-
-    const messages = Array.from({ length: Math.floor(Math.random() * 10) }, () => ({
-      id: faker.string.uuid(),
-      from: faker.internet.email(),
-      subject: faker.lorem.sentence(),
-      body: faker.lorem.paragraph(),
-      received: Math.floor(faker.date.past().getTime() / 1000),
-    }));
-
     ctx.body = {
       status: 'ok',
       timestamp: Math.floor(Date.now() / 1000),
